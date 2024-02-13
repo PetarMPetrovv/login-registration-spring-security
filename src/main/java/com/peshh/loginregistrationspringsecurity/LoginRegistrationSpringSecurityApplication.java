31dd7fe51c92078a4c1a85dd88489be2ca52857d
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-02-13 23:05:58 +0200

    initial commit

---
+++ src/main/java/com/peshh/loginregistrationspringsecurity/LoginRegistrationSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.peshh.loginregistrationspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationSpringSecurityApplication.class, args);
+	}
+
+}
