e91960bf5a2e03c49ae18f627f02d8d98940d977
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-02-24 17:33:10 +0200

    added method for searching if mail is already registered

--- src/main/java/com/peshh/loginregistrationspringsecurity/repository/UserRepository.java
+++ src/main/java/com/peshh/loginregistrationspringsecurity/repository/UserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsernameOrEmail(String username, String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUserName(String username);
 }
