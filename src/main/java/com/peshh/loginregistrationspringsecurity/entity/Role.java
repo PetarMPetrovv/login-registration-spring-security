3d32f2a9dfaf34d553a275839d4294fc48656f1e
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-04-12 18:37:01 +0300

    working on user and role classes anotations

--- src/main/java/com/peshh/loginregistrationspringsecurity/entity/Role.java
+++ src/main/java/com/peshh/loginregistrationspringsecurity/entity/Role.java
@@ -5,10 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
 @Setter
 @Getter
 @AllArgsConstructor
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 }
--- src/main/java/com/peshh/loginregistrationspringsecurity/entity/User.java
+++ src/main/java/com/peshh/loginregistrationspringsecurity/entity/User.java
@@ -6,20 +6,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String userName;
     private String email;
     private String password;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
     private Role role;
 }
