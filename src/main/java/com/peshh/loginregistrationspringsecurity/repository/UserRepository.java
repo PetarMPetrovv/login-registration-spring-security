2c0ef23d7c0747809b4d927d2e4cf740952a3a97
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-04-15 20:32:15 +0300

    added search by email in user repository

--- src/main/java/com/peshh/loginregistrationspringsecurity/repository/UserRepository.java
+++ src/main/java/com/peshh/loginregistrationspringsecurity/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.peshh.loginregistrationspringsecurity.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String username, String email);
+    User findByEmail(String email);
 
     boolean existsByEmail(String email);
 
